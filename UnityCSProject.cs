4055cb791e5768947c4b85ffc1361bf47a582aef
Yemi <yemiry@163.com>
2018-04-23 21:16:11 +0800

    Change namespace to Unity.CSProject

--- UnityCSProject.cs
+++ UnityCSProject.cs
@@ -4,7 +4,7 @@ using System.Xml.Linq;
 using System.IO;
 using System.Collections.Generic;
 
-namespace UnityCSProjectParser
+namespace Unity.CSProject
 {
     public class UnityCSProject
     {
--- UnitySolution.cs
+++ UnitySolution.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 
-namespace UnityCSProjectParser
+namespace Unity.CSProject
 {
     public class UnitySolution{
 
